b0e802809dc5811426500dfc4cda9406eb26c379
Christoph Johns <christophjohns@gmail.com>
2022-03-15 15:08:21 +0100

    Add plugin id

--- src/constants.ts
+++ src/constants.ts
@@ -9,7 +9,8 @@ export const FOCUS_NODE = "FOCUS_NODE";
 export const GET_APPLICATION_STATE = "GET_APPLICATION_STATE";
 export const APPLICATION_STATE = "APPLICATION_STATE";
 
-export const BASE_URL = "http://127.0.0.1:5000"
+// export const BASE_URL = "http://127.0.0.1:4000"
+export const BASE_URL = "https://link-optimizer.herokuapp.com"
 
 export const STATIC = "STATIC";
 export const INTERACTIVE = "INTERACTIVE";
